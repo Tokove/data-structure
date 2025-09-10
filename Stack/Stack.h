af0b4308f3f9611da74cac46c6f3a6dad6ee115a
zihan zhou <2794656116@qq.com>
2025-09-10 12:11:38 +0800

    Update Stack.h

--- Stack/Stack.h
+++ Stack/Stack.h
@@ -1,1 +1,23 @@
 // 栈
+
+// 创建节点
+struct Node
+{
+    int data;
+    Node* link;
+};
+
+class Stack
+{
+private:
+    Node* top;
+    int size;
+public:
+    Stack():top(nullptr){}
+    ~Stack();
+    void Push(int val);
+    void Pop();
+    int Top();
+    int Size()const;
+    bool Empty()const;
+};

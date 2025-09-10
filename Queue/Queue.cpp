0718a7b91e0de89fec5a942887f2c5ef1404e216
zihan zhou <2794656116@qq.com>
2025-09-10 13:35:56 +0800

    Update Queue.cpp

--- Queue/Queue.cpp
+++ Queue/Queue.cpp
@@ -1,1 +1,55 @@
+#include"Queue.h"
+#include<iostream>
 
+void Queue::Push(int val){
+    Node* temp = new Node();
+    temp->data = val;
+    temp->link = nullptr;
+    if(front == nullptr && back == nullptr){ // 等价与 front == nullptr
+        front = back = temp;
+        return ;
+    }
+    back->link = temp;
+    back = temp;
+    size++;
+}
+
+void Queue::Pop(){
+    if(front == nullptr){
+        throw std::out_of_range("Queue is empty");
+    }
+    Node* temp = front;
+    if(front == back){
+        front = back = nullptr;
+    }else{
+        front = front->link;
+    }
+    delete temp;
+    size--;
+}
+
+int Queue::Front() const {
+    if(front == nullptr){
+        throw std::out_of_range("Queue is empty");
+    }
+    return front->data;
+}
+
+int Queue::Back() const {
+    if(back == nullptr){
+        throw std::out_of_range("Queue is empty");
+    }
+    return back->data;
+}
+
+bool Queue::Empty() const { return front == nullptr; }
+
+int Queue::Size() const { return size; }
+
+Queue::~Queue(){
+    while(front){
+        Node* temp = front;
+        front = front->link;
+        delete temp;
+    }
+}

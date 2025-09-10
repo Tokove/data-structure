ce726a551cce7f3763ada5c93759e3e40f713503
zihan zhou <2794656116@qq.com>
2025-09-10 13:34:57 +0800

    Update Stack.cpp

--- Stack/Stack.cpp
+++ Stack/Stack.cpp
@@ -10,26 +10,26 @@ void Stack::Push(int val){
 }
 
 void Stack::Pop(){
-    if(top == nullptr) return ;
+    if(top == nullptr){
+        throw std::out_of_range("Stack is empty");
+    }
     int val = top->data;
     Node* temp = top;
     top = top->link;
     delete temp;
     size--;
 }
-int Stack::Top(){
+
+int Stack::Top() const {
     if(top == nullptr){
         throw std::out_of_range("Stack is empty");
     }
     return top->data;
 }
 
-int Stack::Size() const {
-    return size;
-}
-bool Stack::Empty() const {
-    return top == nullptr;
-}
+int Stack::Size() const { return size; }
+
+bool Stack::Empty() const { return top == nullptr; }
 
 Stack::~Stack(){
     while(top){

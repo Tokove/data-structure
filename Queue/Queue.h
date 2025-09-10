07b13e979350864cb545ee91c242ea656139d4c8
zihan zhou <2794656116@qq.com>
2025-09-10 13:35:35 +0800

    Update Queue.h

--- Queue/Queue.h
+++ Queue/Queue.h
@@ -1,1 +1,25 @@
 // 队列
+
+// 创建节点
+struct Node
+{
+    int data;
+    Node* link;
+};
+
+class Queue
+{
+private:
+    Node* front;
+    Node* back;
+    int size;
+public:
+    Queue():front(nullptr), back(nullptr), size(0){}
+    ~Queue();
+    void Push(int val);
+    void Pop();
+    int Front() const;
+    int Back() const;
+    bool Empty() const;
+    int Size() const;
+};

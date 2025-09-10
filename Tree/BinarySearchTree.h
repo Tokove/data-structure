ca75c85d75a2412e9cb7be06a96f96a05ec5e621
zihan zhou <2794656116@qq.com>
2025-09-10 21:50:22 +0800

    Update BinarySearchTree.h

--- Tree/BinarySearchTree.h
+++ Tree/BinarySearchTree.h
@@ -1,1 +1,19 @@
 // 二叉搜索树
+#pragma once
+// 创建节点
+struct BstNode
+{
+    int data;
+    BstNode* left;  // 左子节点
+    BstNode* right; // 右子节点
+};
+
+class BinarySearchTree
+{
+private:
+    BstNode* root;
+public:
+    BinarySearchTree():root(nullptr){}
+    BstNode* Insert(BstNode* root, int val);
+    bool Search(BstNode* root, int val) const;
+};

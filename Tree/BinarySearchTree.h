bbc55b7a25302f7a5d4ac9e135c4051659f23a4f
zihan zhou <2794656116@qq.com>
2025-09-11 10:06:51 +0800

    add private achieve and pub interface

--- Tree/BinarySearchTree.h
+++ Tree/BinarySearchTree.h
@@ -12,8 +12,11 @@ class BinarySearchTree
 {
 private:
     BstNode* root;
-public:
-    BinarySearchTree():root(nullptr){}
     BstNode* Insert(BstNode* root, int val);
     bool Search(BstNode* root, int val) const;
+public:
+    BinarySearchTree():root(nullptr){}
+    ~BinarySearchTree();
+    void Insert(int val);
+    bool Search(int val) const;
 };

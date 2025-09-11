5679826efbfe37c06ea21aef3fecf89a730c111b
zihan zhou <2794656116@qq.com>
2025-09-11 12:06:36 +0800

    Update linked_list.h

--- linked_list/linked_list.h
+++ linked_list/linked_list.h
@@ -1,1 +1,22 @@
+#pragma once
+// Create a node
+struct Node 
+{
+	int data;
+	Node* next;
+};
 
+class LinkedList
+{
+private:
+	Node* head;
+	int size;
+public:
+	LinkedList() : head(nullptr), size(0) {}
+	~LinkedList();
+	void Insert(int value);
+	void Insert(int index, int value);
+	void Delete(int index);
+	void Print()const;
+	void Reverse();
+};

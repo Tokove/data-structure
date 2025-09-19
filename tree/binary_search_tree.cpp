a3e3922e76b5a64db5a3eba05d038d791460b059
zihan zhou <2794656116@qq.com>
2025-09-19 23:37:10 +0800

    add LevelOrder function

--- tree/binary_search_tree.cpp
+++ tree/binary_search_tree.cpp
@@ -34,3 +34,18 @@ int BinarySearchTree::FindMax(BstNode* root) const {
 }
 
 int BinarySearchTree::FindMax() const { return FindMax(root); }
+
+void BinarySearchTree::LevelOrder() const {
+    if(root == nullptr) return;
+    std::queue<BstNode*> q;
+    q.push(root);
+    while(q.size())
+    {
+        auto t = q.front();
+        q.pop();
+        std::cout << t->data << ' ';
+        if(t->left) q.push(t->left);
+        if(t->right) q.push(t->right);
+    }
+    std::cout << std::endl;
+}
